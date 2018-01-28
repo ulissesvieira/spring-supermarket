c80927a869f720c35503503f86a7805bf412a1e4
Ulisses Vieira <sr.jvieira03@gmail.com>
2018-01-28 12:23:57 -0300

    Inclusão da data de aniversário

--- src/main/java/com/springsupermarket/entity/Client.java
+++ src/main/java/com/springsupermarket/entity/Client.java
@@ -2,16 +2,20 @@ package com.springsupermarket.entity;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
 @Document(collection = "#{clientsRepository.getCollectionName()}")
 public class Client {
 	private Integer id;
 	private String name;
+	private Date birthday;
 
 	public Client() {}
 
-	public Client(Integer id, String name) {
+	public Client(Integer id, String name, Date birthday) {
 		this.id = id;
 		this.name = name;
+		this.birthday = birthday;
 	}
 
 	public Integer getId() {
@@ -25,5 +29,11 @@ public class Client {
 	}
 	public void setName(String name) {
 		this.name = name;
-	}	
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
 }
