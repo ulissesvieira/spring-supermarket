82c6f6463a61d95401b575bd215ddb12db176797
Ulisses Vieira <sr.jvieira03@gmail.com>
2018-02-03 09:44:20 -0300

    Refatoração da paginação

--- src/main/java/com/springsupermarket/mongo/service/AddressesServiceImpl.java
+++ src/main/java/com/springsupermarket/mongo/service/AddressesServiceImpl.java
@@ -63,40 +63,6 @@ public class AddressesServiceImpl implements AddressesService {
         Pageable pageable = new PageRequest(settings.getCurrentPage().intValue() - 1, settings.getItemsPerPage().intValue());
         Page<Address> page = addressesRepository.findAll(pageable);
 
-        int startPage, endPage;
-        int currentPage = settings.getCurrentPage();
-        if (page.getTotalPages() <= 10) {
-            // less than 10 total pages so show all
-            startPage = 1;
-            endPage = page.getTotalPages();
-        } else {
-            // more than 10 total pages so calculate start and end pages
-            if (currentPage <= 6) {
-                startPage = 1;
-                endPage = 10;
-            } else if (currentPage + 4 >= page.getTotalPages()) {
-                startPage = page.getTotalPages() - 9;
-                endPage = page.getTotalPages();
-            } else {
-                startPage = currentPage - 5;
-                endPage = currentPage + 4;
-            }
-        }
-
-        ArrayList<Integer> pages = new ArrayList<>();
-        for (int i = startPage; i < endPage + 1; i++)
-            pages.add(i);
-
-        PaginationResult<Address> result = new PaginationResult<>();
-        result.setItems(page.getContent());
-        result.setTotalItems(page.getTotalElements());
-        result.setTotalPages(page.getTotalPages());
-        result.setCurrentPage(currentPage);
-        result.setStartPage(startPage);
-        result.setEndPage(endPage);
-        result.setPageSize(settings.getItemsPerPage());
-        result.setPages(pages.toArray(new Integer[pages.size()]));
-
-        return result;
+        return new PaginationResult<Address>(page);
     }
 }
--- src/main/java/com/springsupermarket/mongo/utils/PaginationResult.java
+++ src/main/java/com/springsupermarket/mongo/utils/PaginationResult.java
@@ -1,5 +1,8 @@
 package com.springsupermarket.mongo.utils;
 
+import org.springframework.data.domain.Page;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class PaginationResult <T> {
@@ -12,67 +15,59 @@ public class PaginationResult <T> {
     private Integer[] pages;
     List<T> items;
 
+    public PaginationResult(Page<T> page) {
+        items = page.getContent();
+        totalPages = page.getTotalPages();
+        totalItems = page.getTotalElements();
+        currentPage = page.getNumber() + 1;
+        pageSize = page.getSize();
+
+        if (totalPages <= 10) {
+            // less than 10 total pages so show all
+            startPage = 1;
+            endPage = totalPages;
+        } else {
+            // more than 10 total pages so calculate start and end pages
+            if (currentPage <= 6) {
+                startPage = 1;
+                endPage = 10;
+            } else if (currentPage + 4 >= totalPages) {
+                startPage = totalPages - 9;
+                endPage = totalPages;
+            } else {
+                startPage = currentPage - 5;
+                endPage = currentPage + 4;
+            }
+        }
+
+        ArrayList<Integer> pages = new ArrayList<>();
+        for (int i = startPage; i < endPage + 1; i++)
+            pages.add(i);
+        this.pages = pages.toArray(new Integer[pages.size()]);
+    }
+
     public Long getTotalItems() {
         return totalItems;
     }
-
-    public void setTotalItems(Long totalItems) {
-        this.totalItems = totalItems;
-    }
-
     public Integer getCurrentPage() {
         return currentPage;
     }
-
-    public void setCurrentPage(Integer currentPage) {
-        this.currentPage = currentPage;
-    }
-
     public Integer getPageSize() {
         return pageSize;
     }
-
-    public void setPageSize(Integer pageSize) {
-        this.pageSize = pageSize;
-    }
-
     public Integer getTotalPages() {
         return totalPages;
     }
-
-    public void setTotalPages(Integer totalPages) {
-        this.totalPages = totalPages;
-    }
-
     public Integer getStartPage() {
         return startPage;
     }
-
-    public void setStartPage(Integer startPage) {
-        this.startPage = startPage;
-    }
-
     public Integer getEndPage() {
         return endPage;
     }
-
-    public void setEndPage(Integer endPage) {
-        this.endPage = endPage;
-    }
-
     public Integer[] getPages() {
         return pages;
     }
-
-    public void setPages(Integer[] pages) {
-        this.pages = pages;
-    }
-
     public List<T> getItems() {
         return items;
-    }
-
-    public void setItems(List<T> items) {
-        this.items = items;
     }
 }
